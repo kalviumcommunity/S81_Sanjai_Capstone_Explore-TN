2e7486ed009413c2669220e926ce82c6b8b7f0ad
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-05-01 21:51:39 +0530

    SchemaAdded

---
+++ Back-End/models/userModel.js
@@ -0,0 +1,35 @@
+const  mongoose  = require("mongoose")
+
+
+const userSchema=mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    isActivated: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+
+const userModel=mongoose.model("user",userSchema)
+
+module.exports=userModel
