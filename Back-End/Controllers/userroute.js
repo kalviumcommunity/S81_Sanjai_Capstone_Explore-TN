928541c40cef871272e04ed9319b0b853d506b8f
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-05-02 16:33:32 +0530

    ControllersDELETEAdded

--- Back-End/Controllers/userroute.js
+++ Back-End/Controllers/userroute.js
@@ -34,7 +34,7 @@ router.post("/login", catchAsyncError(async (req, res, next) => {
   const user = await User.findOne({ email });
   if (!user) {
     return res.status(404).json({ message: "User not found" });
-  }
+  }   
 
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
@@ -42,6 +42,16 @@ router.post("/login", catchAsyncError(async (req, res, next) => {
   }
 
   res.status(200).json({ message: "Login successful" });
+}));
+
+router.delete("/user/:id", catchAsyncError(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.status(200).json({ message: "User deleted successfully" });
 }));
 
 module.exports = router;
