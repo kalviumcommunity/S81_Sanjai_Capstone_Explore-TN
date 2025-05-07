d2d7511fa2724a5e4f32d8c4dc003cdd9668bf2f
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-05-07 17:48:07 +0530

    Componants ADDED

---
+++ Front-End/componants/DestinationCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { LuSquareArrowOutUpRight } from "react-icons/lu";
+import { useNavigate } from "react-router-dom";
+
+const DestinationCard = () => {
+    return (
+        <div className=" container mx-auto px-6 py-3">
+            <h2 className="text-3xl font-bold mb-2.5 text-gray-100 text-left">
+                Popular Destinations in TAMIL NADU
+            </h2>
+        </div>
+    );
+};
+
+export default DestinationCard;
